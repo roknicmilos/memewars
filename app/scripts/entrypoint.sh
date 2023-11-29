9962fe5e0ca68dadf7bd1231aeeaa2f248d37d88
Milos Roknic <m.roknic@vegait.rs>
2023-11-29 17:19:14 +0100

    Move packages installation from Dockerfile to entrypoint.sh for development target

--- app/scripts/entrypoint.sh
+++ app/scripts/entrypoint.sh
@@ -8,6 +8,8 @@ export REACT_APP_APP_URL=$APP_BASE_URL
 export REACT_APP_API_URL="$API_BASE_URL/api"
 
 if [ "$1" = "dev" ]; then
+  printc "Installing packages...\n" "info"
+  npm install
   printc "Starting DEVELOPMENT server...\n" "info"
   npm start
 
